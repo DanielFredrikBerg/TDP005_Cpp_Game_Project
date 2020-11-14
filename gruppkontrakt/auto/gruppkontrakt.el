8145744b7b5437875ebf927ecadef9197bfeda15
danhu849 <danhu849@gitlab.liu.se>
2020-11-14 16:19:42 +0100

    gruppkontrakt klart, men ej rättat

--- gruppkontrakt/auto/gruppkontrakt.el
+++ gruppkontrakt/auto/gruppkontrakt.el
@@ -1,10 +1,13 @@
 (TeX-add-style-hook
  "gruppkontrakt"
  (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8")))
    (TeX-run-style-hooks
     "latex2e"
     "mall"
-    "mall10")
+    "mall10"
+    "inputenc")
    (TeX-add-symbols
     "version")
    (LaTeX-add-labels
