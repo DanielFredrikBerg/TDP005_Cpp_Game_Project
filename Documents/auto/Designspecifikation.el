4251b1212315d5c9edf4f9b4461a831a382d6b57
danhu849 <danhu849@gitlab.liu.se>
2020-11-26 19:51:10 +0100

    added pics

--- Documents/auto/Designspecifikation.el
+++ Documents/auto/Designspecifikation.el
@@ -3,10 +3,10 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("babel" "swedish") ("adjustbox" "export") ("biblatex" "style=authoryear" "backend=biber")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
    (TeX-run-style-hooks
     "latex2e"
     "TDP005mall"
@@ -20,6 +20,8 @@
     "csquotes")
    (TeX-add-symbols
     "version")
+   (LaTeX-add-labels
+    "fig:1")
    (LaTeX-add-bibliographies
     "reference"))
  :latex)
