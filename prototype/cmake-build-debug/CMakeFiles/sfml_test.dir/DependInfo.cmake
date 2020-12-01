cc657d99c79b8ed3d364ff799c1c7ce841c840b2
danhu849 <danhu849@gitlab.liu.se>
2020-12-01 09:49:21 +0100

    enemies now have both angles

--- prototype/cmake-build-debug/CMakeFiles/sfml_test.dir/DependInfo.cmake
+++ prototype/cmake-build-debug/CMakeFiles/sfml_test.dir/DependInfo.cmake
@@ -4,19 +4,19 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/viktor/CLionProjects/tdp005/prototype/attack_behavior.cpp" "/home/viktor/CLionProjects/tdp005/prototype/cmake-build-debug/CMakeFiles/sfml_test.dir/attack_behavior.cpp.o"
-  "/home/viktor/CLionProjects/tdp005/prototype/component.cpp" "/home/viktor/CLionProjects/tdp005/prototype/cmake-build-debug/CMakeFiles/sfml_test.dir/component.cpp.o"
-  "/home/viktor/CLionProjects/tdp005/prototype/enemy.cpp" "/home/viktor/CLionProjects/tdp005/prototype/cmake-build-debug/CMakeFiles/sfml_test.dir/enemy.cpp.o"
-  "/home/viktor/CLionProjects/tdp005/prototype/game_object.cpp" "/home/viktor/CLionProjects/tdp005/prototype/cmake-build-debug/CMakeFiles/sfml_test.dir/game_object.cpp.o"
-  "/home/viktor/CLionProjects/tdp005/prototype/game_state.cpp" "/home/viktor/CLionProjects/tdp005/prototype/cmake-build-debug/CMakeFiles/sfml_test.dir/game_state.cpp.o"
-  "/home/viktor/CLionProjects/tdp005/prototype/level.cpp" "/home/viktor/CLionProjects/tdp005/prototype/cmake-build-debug/CMakeFiles/sfml_test.dir/level.cpp.o"
-  "/home/viktor/CLionProjects/tdp005/prototype/main.cpp" "/home/viktor/CLionProjects/tdp005/prototype/cmake-build-debug/CMakeFiles/sfml_test.dir/main.cpp.o"
-  "/home/viktor/CLionProjects/tdp005/prototype/menu_state.cpp" "/home/viktor/CLionProjects/tdp005/prototype/cmake-build-debug/CMakeFiles/sfml_test.dir/menu_state.cpp.o"
-  "/home/viktor/CLionProjects/tdp005/prototype/movement_behavior.cpp" "/home/viktor/CLionProjects/tdp005/prototype/cmake-build-debug/CMakeFiles/sfml_test.dir/movement_behavior.cpp.o"
-  "/home/viktor/CLionProjects/tdp005/prototype/moving_object.cpp" "/home/viktor/CLionProjects/tdp005/prototype/cmake-build-debug/CMakeFiles/sfml_test.dir/moving_object.cpp.o"
-  "/home/viktor/CLionProjects/tdp005/prototype/player.cpp" "/home/viktor/CLionProjects/tdp005/prototype/cmake-build-debug/CMakeFiles/sfml_test.dir/player.cpp.o"
-  "/home/viktor/CLionProjects/tdp005/prototype/projectile.cpp" "/home/viktor/CLionProjects/tdp005/prototype/cmake-build-debug/CMakeFiles/sfml_test.dir/projectile.cpp.o"
-  "/home/viktor/CLionProjects/tdp005/prototype/state.cpp" "/home/viktor/CLionProjects/tdp005/prototype/cmake-build-debug/CMakeFiles/sfml_test.dir/state.cpp.o"
+  "/home/danhu849/tdp005/prototype/attack_behavior.cpp" "/home/danhu849/tdp005/prototype/cmake-build-debug/CMakeFiles/sfml_test.dir/attack_behavior.cpp.o"
+  "/home/danhu849/tdp005/prototype/component.cpp" "/home/danhu849/tdp005/prototype/cmake-build-debug/CMakeFiles/sfml_test.dir/component.cpp.o"
+  "/home/danhu849/tdp005/prototype/enemy.cpp" "/home/danhu849/tdp005/prototype/cmake-build-debug/CMakeFiles/sfml_test.dir/enemy.cpp.o"
+  "/home/danhu849/tdp005/prototype/game_object.cpp" "/home/danhu849/tdp005/prototype/cmake-build-debug/CMakeFiles/sfml_test.dir/game_object.cpp.o"
+  "/home/danhu849/tdp005/prototype/game_state.cpp" "/home/danhu849/tdp005/prototype/cmake-build-debug/CMakeFiles/sfml_test.dir/game_state.cpp.o"
+  "/home/danhu849/tdp005/prototype/level.cpp" "/home/danhu849/tdp005/prototype/cmake-build-debug/CMakeFiles/sfml_test.dir/level.cpp.o"
+  "/home/danhu849/tdp005/prototype/main.cpp" "/home/danhu849/tdp005/prototype/cmake-build-debug/CMakeFiles/sfml_test.dir/main.cpp.o"
+  "/home/danhu849/tdp005/prototype/menu_state.cpp" "/home/danhu849/tdp005/prototype/cmake-build-debug/CMakeFiles/sfml_test.dir/menu_state.cpp.o"
+  "/home/danhu849/tdp005/prototype/movement_behavior.cpp" "/home/danhu849/tdp005/prototype/cmake-build-debug/CMakeFiles/sfml_test.dir/movement_behavior.cpp.o"
+  "/home/danhu849/tdp005/prototype/moving_object.cpp" "/home/danhu849/tdp005/prototype/cmake-build-debug/CMakeFiles/sfml_test.dir/moving_object.cpp.o"
+  "/home/danhu849/tdp005/prototype/player.cpp" "/home/danhu849/tdp005/prototype/cmake-build-debug/CMakeFiles/sfml_test.dir/player.cpp.o"
+  "/home/danhu849/tdp005/prototype/projectile.cpp" "/home/danhu849/tdp005/prototype/cmake-build-debug/CMakeFiles/sfml_test.dir/projectile.cpp.o"
+  "/home/danhu849/tdp005/prototype/state.cpp" "/home/danhu849/tdp005/prototype/cmake-build-debug/CMakeFiles/sfml_test.dir/state.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
