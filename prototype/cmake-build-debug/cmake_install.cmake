e61c1b9fdd7d575e646839f01282ac1185e59f56
viktor <vikro653@student.liu.se>
2020-12-04 20:38:30 +0100

    revert something

--- prototype/cmake-build-debug/cmake_install.cmake
+++ prototype/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/danhu849/tdp005/prototype
+# Install script for directory: /home/viktor/CLionProjects/tdp005/prototype
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/danhu849/tdp005/prototype/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/viktor/CLionProjects/tdp005/prototype/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
