4f6019c55d8253d8d7739a602490761be4e62ae9
danhu849 <danhu849@gitlab.liu.se>
2020-12-14 14:20:55 +0100

    updated kravspec to latest V

--- Documents/auto/Kravspecifikation.el
+++ Documents/auto/Kravspecifikation.el
@@ -2,11 +2,11 @@
  "Kravspecifikation"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("inputenc" "utf8") ("babel" "swedish") ("adjustbox" "export") ("biblatex" "style=authoryear" "backend=biber")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+                     '(("inputenc" "utf8") ("babel" "swedish") ("adjustbox" "export") ("ulem" "normalem") ("biblatex" "style=authoryear" "backend=biber")))
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
    (TeX-run-style-hooks
     "latex2e"
     "TDP005mall"
@@ -14,6 +14,7 @@
     "inputenc"
     "babel"
     "adjustbox"
+    "ulem"
     "tabularx"
     "caption"
     "biblatex"
